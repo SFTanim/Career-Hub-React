bad65d9c47f515549de6a2a801be78ab0877767e
Sabid Farhan <sfttanim@gmail.com>
2024-03-24 22:30:51 +0600

    Navlink added

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,33 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './components/Root/Root';
 import Home from './components/Home/Home';
+import Statistics from './components/NavigationLink/Statistics';
+import AppliedJobs from './components/Navbar/AppliedJobs';
+import JobDetails from './components/Navbar/JobDetails';
+import ErrorElement from './Error/ErrorElement';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
+    errorElement:<ErrorElement></ErrorElement>,
     children: [
       {
         path: '/',
         element: <Home></Home>,
-      }
+      },
+      {
+        path:'/statistics',
+        element:<Statistics></Statistics>,
+      },
+      {
+        path:'/appliedJobs',
+        element:<AppliedJobs></AppliedJobs>,
+      },
+      {
+        path:'/jobDetails',
+        element:<JobDetails></JobDetails>,
+      },
     ]
   }
 ])
